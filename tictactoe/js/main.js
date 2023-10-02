93a52ccc711187b2bc8fb2f6ae4f80cd606d5833
asantiagoNick <127116251+asantiagoNick@users.noreply.github.com>
2023-10-02 10:51:31 -0300

    Add files via upload

---
+++ tictactoe/js/main.js
@@ -0,0 +1,74 @@
+	letra = "X";
+ 
+	function joga(celula){
+		let celulaclicada = document.getElementById(celula).innerHTML;
+		if (celulaclicada == "X" || celulaclicada == "O"){
+			alert("Opa, este quadrado já foi escolhido!");
+		}else{
+			document.getElementById(celula).innerHTML = letra;
+			if (letra == "X"){
+				letra = "O";
+			}else{
+				letra = "X";
+			}
+		}
+	}
+	function testa(){
+		const c1 = document.getElementById('cel11').innerHTML;
+		const c2 = document.getElementById('cel12').innerHTML;
+		const c3 = document.getElementById('cel13').innerHTML;
+		const c4 = document.getElementById('cel21').innerHTML;
+		const c5 = document.getElementById('cel22').innerHTML;
+		const c6 = document.getElementById('cel23').innerHTML;
+		const c7 = document.getElementById('cel31').innerHTML;
+		const c8 = document.getElementById('cel32').innerHTML;
+		const c9 = document.getElementById('cel33').innerHTML;
+		
+		if(
+	    c1 != '' && c4 != '' && c7 != ''&&
+		c1 == c4 && c4 == c7 || //testar primeira coluna 
+
+			//testar segunda coluna 
+		c2 != '' && c5 != '' && c8 != '' &&
+		c2 == c5 && c5 == c8 || 
+
+		//testar terceira coluna 
+		c3 != '' && c6 != '' && c9 != '' && 
+		c3 == c6 && c6 == c9 ||
+
+		//testar primeira linha 
+		c1 != '' && c2 != '' && c3 != '' && 
+		c1 == c2 && c2 == c3 ||
+
+		//testar segunda linha 
+		c4 != '' && c5 != '' && c6 != '' && 
+		c4 == c5 && c5 == c6 ||
+
+		//testar terceira linha 
+		c7 != '' && c8 != '' && c9 != '' && 
+		c7 == c8 && c8 == c9 ||
+
+		//testar primeira diagonal 
+		c1 != ''&& c5 != '' && c9 != '' && 
+		c1== c5 && c5 == c9 ||
+
+		//testar segunda diagonal 
+		c3 != '' && c5 != '' && c7 != '' && 
+		c3 == c5 && c5 == c7 )
+
+		{
+			alert('Você venceu!!!');
+			limpa ();
+		}
+	}
+ function limpa (){
+	c1 = document.getElementById('cel11').innerHTML = '';
+	c2 = document.getElementById('cel12').innerHTML = '';
+	c3 = document.getElementById('cel13').innerHTML = '';
+	c4 = document.getElementById('cel21').innerHTML = '';
+	c5 = document.getElementById('cel22').innerHTML = '';
+	c6 = document.getElementById('cel23').innerHTML = '';
+    c7 = document.getElementById('cel31').innerHTML = '';
+	c8 = document.getElementById('cel32').innerHTML = '';
+	c9 = document.getElementById('cel33').innerHTML = '';
+		}
